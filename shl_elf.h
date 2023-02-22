db9266ee2bac0b530c98b9d822acdf7148e6f1df
Serhii Ostrikov <serostrikov@gmail.com>
2023-02-22 18:00:41 +0200

    more defines

--- shl_elf.h
+++ shl_elf.h
@@ -45,50 +45,53 @@ typedef Elf32_Sword	elf32_sword;
 typedef Elf32_Word	elf32_word;
 typedef Elf32_Half	elf32_half;
 
-#define ET_NONE   0 	 /* No file type */
-#define ET_REL 	  1 	 /* Relocatable file */
-#define ET_EXEC   2 	 /* Executable file */
-#define ET_DYN 	  3 	 /* Shared object file */
-#define ET_CORE   4 	 /* Core file */
-#define ET_LOPROC 0xff00 /* Processor-specific */
-#define ET_HIPROC 0xffff /* Processor-specific */
+#define ET_NONE   	0 	 /* No file type */
+#define ET_REL 	  	1 	 /* Relocatable file */
+#define ET_EXEC   	2 	 /* Executable file */
+#define ET_DYN 	  	3 	 /* Shared object file */
+#define ET_CORE   	4 	 /* Core file */
+#define ET_LOPROC 	0xff00 /* Processor-specific */
+#define ET_HIPROC 	0xffff /* Processor-specific */
 
-#define ET_NONE  0  /* No machine */
-#define EM_M32 	 1  /* AT&T WE 32100 */
-#define EM_SPARC 2  /* SPARC */
-#define EM_386 	 3  /* Intel Architecture */
-#define EM_68K 	 4  /* Motorola 68000 */
-#define EM_88K 	 5  /* Motorola 88000 */
-#define EM_860 	 7  /* Intel 80860 */
-#define EM_MIPS	 8  /* MIPS RS3000 Big-Endian */
-#define EM_MIPS_RS4_BE 	10 /* MIPS RS4000 Big-Endian */
+#define ET_NONE  	0  /* No machine */
+#define EM_M32 	 	1  /* AT&T WE 32100 */
+#define EM_SPARC 	2  /* SPARC */
+#define EM_386 	 	3  /* Intel Architecture */
+#define EM_68K 	 	4  /* Motorola 68000 */
+#define EM_88K 	 	5  /* Motorola 88000 */
+#define EM_860 	 	7  /* Intel 80860 */
+#define EM_MIPS	 	8  /* MIPS RS3000 Big-Endian */
+#define EM_MIPS_RS4_BE	10 /* MIPS RS4000 Big-Endian */
 /* RESERVED 11-16 */
 
-#define EV_NONE    0 /* Invalid versionn */
-#define EV_CURRENT 1 /* Current version */
+#define EV_NONE    	0 /* Invalid versionn */
+#define EV_CURRENT 	1 /* Current version */
 
-#define EI_MAG0  0 	/* File identification */
-#define EI_MAG1  1 	/* File identification */ 
-#define EI_MAG2  2 	/* File identification */
-#define EI_MAG3  3 	/* File identification */
-#define EI_CLASS 4	/* File class */
-#define EI_DATA  5 	/* Data encoding */
-#define EI_PAD   7 	/* Start of padding bytes */
-#define EI_VERSION 6 	/* File version */
-#define EI_NIDENT  16	/* Size of e_ident[] */
+#define EI_MAG0  	0 	/* File identification */
+#define EI_MAG1  	1 	/* File identification */ 
+#define EI_MAG2  	2 	/* File identification */
+#define EI_MAG3  	3 	/* File identification */
+#define EI_CLASS 	4	/* File class */
+#define EI_DATA  	5 	/* Data encoding */
+#define EI_PAD   	7 	/* Start of padding bytes */
+#define EI_VERSION 	6 	/* File version */
+#define EI_NIDENT  	16	/* Size of e_ident[] */
 
-#define ELFMAG0 0x7f /* e_ident[EI_MAG0] */
-#define ELFMAG1 ’E’  /* e_ident[EI_MAG1] */
-#define ELFMAG2 ’L’  /* e_ident[EI_MAG2] */
-#define ELFMAG3 ’F’  /* e_ident[EI_MAG3] */
+#define ELFMAG0 	0x7f /* e_ident[EI_MAG0] */
+#define ELFMAG1 	’E’  /* e_ident[EI_MAG1] */
+#define ELFMAG2 	’L’  /* e_ident[EI_MAG2] */
+#define ELFMAG3 	’F’  /* e_ident[EI_MAG3] */
 
-#define ELFCLASSNONE 0 /* Invalid class */
-#define ELFCLASS32   1 /* 32-bit objects */
-#define ELFCLASS64   2 /* 64-bit objects */
+#define ELFCLASSNONE 	0 /* Invalid class */
+#define ELFCLASS32   	1 /* 32-bit objects */
+#define ELFCLASS64   	2 /* 64-bit objects */
 
-#define ELFDATANONE 0 /* Invalid data encoding */
-#define ELFDATA2LSB 1 /* See below */
-#define ELFDATA2MSB 2 /* See below */
+#define ELFDATANONE 	0 /* Invalid data encoding */
+#define ELFDATA2LSB 	1
+#define ELFDATA2MSB 	2
+
+#define ELFOSABI_NONE	0
+#define ELFOSABI_LINUX	3
 
 #define SHN_UNDEF 	0 	
 #define SHN_LORESERVE	0xff00	
@@ -122,10 +125,6 @@ typedef Elf32_Half	elf32_half;
 
 #define STN_UNDEF	0 
 
-#define ELF32_ST_BIND(i)   ((i)>>4)
-#define ELF32_ST_TYPE(i)   ((i)&0xf)
-#define ELF32_ST_INFO(b,t) (((b)<<4)+((t)&0xf))
-
 #define STB_LOCAL 	0 
 #define STB_GLOBAL 	1 
 #define STB_WEAK 	2 
@@ -149,6 +148,62 @@ typedef Elf32_Half	elf32_half;
 #define PT_PHDR		6
 #define PT_LOPROC 	0x70000000
 #define PT_HIPROC 	0x7fffffff
+
+#define DT_NULL		0
+#define DT_NEEDED	1
+#define DT_PLTRELSZ	2
+#define DT_PLTGOT	3
+#define DT_HASH		4
+#define DT_STRTAB	5
+#define DT_SYMTAB	6
+#define DT_RELA		7
+#define DT_RELASZ	8
+#define DT_RELAENT	9
+#define DT_STRSZ	10
+#define DT_SYMENT	11
+#define DT_INIT		12
+#define DT_FINI		13
+#define DT_SONAME	14
+#define DT_RPATH	15
+#define DT_SYMBOLIC	16
+#define DT_REL	        17
+#define DT_RELSZ	18
+#define DT_RELENT	19
+#define DT_PLTREL	20
+#define DT_DEBUG	21
+#define DT_TEXTREL	22
+#define DT_JMPREL	23
+#define DT_ENCODING	32
+#define OLD_DT_LOOS	0x60000000
+#define DT_LOOS		0x6000000d
+#define DT_HIOS		0x6ffff000
+#define DT_VALRNGLO	0x6ffffd00
+#define DT_VALRNGHI	0x6ffffdff
+#define DT_ADDRRNGLO	0x6ffffe00
+#define DT_ADDRRNGHI	0x6ffffeff
+#define DT_VERSYM	0x6ffffff0
+#define DT_RELACOUNT	0x6ffffff9
+#define DT_RELCOUNT	0x6ffffffa
+#define DT_FLAGS_1	0x6ffffffb
+#define DT_VERDEF	0x6ffffffc
+#define	DT_VERDEFNUM	0x6ffffffd
+#define DT_VERNEED	0x6ffffffe
+#define	DT_VERNEEDNUM	0x6fffffff
+#define OLD_DT_HIOS     0x6fffffff
+#define DT_LOPROC	0x70000000
+#define DT_HIPROC	0x7fffffff
+
+#define PF_R		0x4
+#define PF_W		0x2
+#define PF_X		0x1
+
+#define ELF32_ST_BIND(i)   	((i)>>4)
+#define ELF32_ST_TYPE(i)   	((i)&0xf)
+#define ELF32_ST_INFO(b,t) 	(((b)<<4)+((t)&0xf))
+
+#define ELF32_R_SYM(i) 		((i)>>8)
+#define ELF32_R_TYPE(i) 	((unsigned char)(i))
+#define ELF32_R_INFO(s,t) 	(((s)<<8)+(unsigned char)(t))
 
 struct elf32_ehdr {
 	unsigned char e_ident[EI_NIDENT];
