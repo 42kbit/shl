221f00fd085582e98804a8bd4d5966a0db8211eb
Serhii Ostrikov <serostrikov@gmail.com>
2023-02-22 19:39:29 +0200

    glibc elf.h defines

--- usage/elf/elf_usage.c
+++ usage/elf/elf_usage.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "../../shl_elf.h"
 
-int main(void){
-    return 0;
+int main(int argc, char* argv[]){
+	if (argc < 2){
+		printf("Usage: elf_usage <filename>\n");
+		return 0;
+	}
+	FILE* file;
+	long fsz;
+	file = fopen(argv[1], "r");
+	if (!file){
+		fprintf(stderr, "cannot open file, perhaps invalid name.\n");
+		return -1;
+	}
+	
+	fseek(file, 0L, SEEK_END);
+	fsz = ftell(file);
+	rewind(file);
+	void* mem = malloc(fsz);
+	if (!mem){
+		fprintf(stderr, "malloc failed, perhaps file is too big.\n");
+		fclose(file);
+		return -1;
+	}
+	fread (mem, fsz, fsz, file);
+	fclose(file);
+	
+	printf("%s\n", (char*)mem);
+	struct  elf32_ehdr* p = mem;
+	printf("%s\n", p->e_ident);
+
+	free(mem);
+	return 0;
 }
