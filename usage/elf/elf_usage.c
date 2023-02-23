1afb4a3de14f04b01a8a54464170fd02aeef9122
Serhii Ostrikov <serostrikov@gmail.com>
2023-02-23 15:45:40 +0200

    elf_usage.c more bullshit

--- usage/elf/elf_usage.c
+++ usage/elf/elf_usage.c
@@ -82,35 +82,105 @@ struct elf_pinfo {
 	void (*to_host_endian)(void* dst, void* src, int size);
 
 	/* Header parsing */
-	void (*elf_phoff)(struct elf_pinfo*, void* buf);
+	void (*elf_e_ident)	(struct elf_pinfo*, void* buf);
+	void (*elf_e_type)	(struct elf_pinfo*, void* buf);	 
+	void (*elf_e_machine)	(struct elf_pinfo*, void* buf);
+	void (*elf_e_version)	(struct elf_pinfo*, void* buf);
+	void (*elf_e_entry)	(struct elf_pinfo*, void* buf);
+	void (*elf_e_phoff)	(struct elf_pinfo*, void* buf);
+	void (*elf_e_shoff)	(struct elf_pinfo*, void* buf);
+	void (*elf_e_flags)	(struct elf_pinfo*, void* buf);
+	void (*elf_e_ehsize)	(struct elf_pinfo*, void* buf);
+	void (*elf_e_phentsize)	(struct elf_pinfo*, void* buf);
+	void (*elf_e_phnum)	(struct elf_pinfo*, void* buf);
+	void (*elf_e_shentsize)	(struct elf_pinfo*, void* buf);
+	void (*elf_e_shnum)	(struct elf_pinfo*, void* buf);
+	void (*elf_e_shstrndx)	(struct elf_pinfo*, void* buf);
+
 };
 
-
-static inline void elf32_phoff(struct elf_pinfo* pinfo, void* buf){
-	void* hdr = pinfo->elf_ehdr;
-	pinfo->to_host_endian(buf, &((struct elf32_ehdr*)hdr)->e_phoff,
-			sizeof(uint32_t));
+/* This macro generates getter functions like this one.
+ *
+ * static inline void elf32_e_phoff(struct elf_pinfo* pinfo, void* buf){
+ * 	void* hdr = pinfo->elf_ehdr;
+ * 	pinfo->to_host_endian(buf, &((struct elf32_ehdr*)hdr)->e_phoff,
+ * 			sizeof(uint32_t));
+ * }
+*/
+#define __gen_elf_hdr_getter(int, bits, element, element_size)				\
+static inline void elf ## bits ## _ ## element(struct elf_pinfo* pinfo, void* buf) {	\
+	void* hdr = pinfo->elf_ehdr;							\
+	pinfo->to_host_endian(buf, &((struct elf ## bits ## _ehdr*)hdr)->element,	\
+			sizeof(int ## element_size ## _t));				\
 }
 
-static inline void elf64_phoff(struct elf_pinfo* pinfo, void* buf){
-	void* hdr = pinfo->elf_ehdr;
-	pinfo->to_host_endian(buf, &((struct elf64_ehdr*)hdr)->e_phoff,
-			sizeof(uint64_t));
+#define __gen_elf_hdr_getter_all_16(int, element)	\
+	__gen_elf_hdr_getter(int, 32, element, 16)	\
+	__gen_elf_hdr_getter(int, 64, element, 16)	
+
+#define __gen_elf_hdr_getter_all(int, element)		\
+	__gen_elf_hdr_getter(int, 32, element, 32)	\
+	__gen_elf_hdr_getter(int, 64, element, 64)
+
+__gen_elf_hdr_getter_all_16(uint, e_type);
+__gen_elf_hdr_getter_all_16(uint, e_machine);
+__gen_elf_hdr_getter_all(uint, e_version);
+__gen_elf_hdr_getter_all(uint, e_entry);
+__gen_elf_hdr_getter_all(uint, e_phoff);
+__gen_elf_hdr_getter_all(uint, e_shoff);
+__gen_elf_hdr_getter_all(uint, e_flags);
+__gen_elf_hdr_getter_all_16(uint, e_ehsize);
+__gen_elf_hdr_getter_all_16(uint, e_phentsize);
+__gen_elf_hdr_getter_all_16(uint, e_phnum);
+__gen_elf_hdr_getter_all_16(uint, e_shentsize);
+__gen_elf_hdr_getter_all_16(uint, e_shnum);
+__gen_elf_hdr_getter_all_16(uint, e_shstrndx); 	
+
+static inline void elf_e_ident(struct elf_pinfo* pinfo, void* buf){
+	memcpy(buf, pinfo->elf_ehdr, EI_NIDENT);
 }
 
 static inline int elf_pinfo_init_from_ehdr(struct elf_pinfo* info, void* mem){
 	info->elf_ehdr = mem;
 	switch (((struct elf32_ehdr*)mem)->e_ident[EI_CLASS]){
 		case ELFCLASS32:
-			info->elf_phoff = elf32_phoff;
+			info->elf_e_phoff	= elf32_e_phoff;
+                        info->elf_e_type	= elf32_e_type;
+                        info->elf_e_machine	= elf32_e_machine;
+                        info->elf_e_version	= elf32_e_version;
+                        info->elf_e_entry	= elf32_e_entry;
+                        info->elf_e_phoff	= elf32_e_phoff;
+                        info->elf_e_shoff	= elf32_e_shoff;
+                        info->elf_e_flags	= elf32_e_flags;
+                        info->elf_e_ehsize	= elf32_e_ehsize;
+                        info->elf_e_phentsize	= elf32_e_phentsize;
+                        info->elf_e_phnum	= elf32_e_phnum;
+                        info->elf_e_shentsize	= elf32_e_shentsize;
+                        info->elf_e_shnum	= elf32_e_shnum;
+                        info->elf_e_shstrndx	= elf32_e_shstrndx;
 			break;
 		case ELFCLASS64:
-			info->elf_phoff = elf64_phoff;
+			info->elf_e_phoff	= elf64_e_phoff;
+                        info->elf_e_type	= elf64_e_type;
+                        info->elf_e_machine	= elf64_e_machine;
+                        info->elf_e_version	= elf64_e_version;
+                        info->elf_e_entry	= elf64_e_entry;
+                        info->elf_e_phoff	= elf64_e_phoff;
+                        info->elf_e_shoff	= elf64_e_shoff;
+                        info->elf_e_flags	= elf64_e_flags;
+                        info->elf_e_ehsize	= elf64_e_ehsize;
+                        info->elf_e_phentsize	= elf64_e_phentsize;
+                        info->elf_e_phnum	= elf64_e_phnum;
+                        info->elf_e_shentsize	= elf64_e_shentsize;
+                        info->elf_e_shnum	= elf64_e_shnum;
+                        info->elf_e_shstrndx	= elf64_e_shstrndx;
+
 			break;
 		case ELFCLASSNONE:
 			return -EINVAL;
 			break;
 	}
+	info->elf_e_ident = elf_e_ident;
 
 	switch (((struct elf32_ehdr*)mem)->e_ident[EI_DATA]){
 		case ELFDATA2LSB:
@@ -154,9 +224,11 @@ int main(int argc, char* argv[]){
 		free(mem);
 		return -1;
 	}
-	uint64_t phoff = 0;
-	ops.elf_phoff(&ops, &phoff);
-	printf("%lu\n", phoff);
+	uint64_t phoff = 0, type = 0, phentsize = 0;
+	ops.elf_e_phoff(&ops, &phoff);
+	ops.elf_e_type (&ops, &type);
+	ops.elf_e_phentsize(&ops, &phentsize);
+	printf("%lu, %p, %lu\n", phoff, (void*)type, phentsize);
 	
 	free(mem);
 	return 0;
