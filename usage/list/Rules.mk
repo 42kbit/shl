f932d829630ee41301ccdbc4b4ed3927d848bc2f
Serhii Ostrikov <serostrikov@gmail.com>
2023-03-12 23:19:02 +0200

    invalid Rules.mk dependency fix

--- usage/list/Rules.mk
+++ usage/list/Rules.mk
@@ -1,23 +1,51 @@
-COBJS_$(d)	:=$(od)/list_usage_unsafe.o
+#COBJS_$(d)	:=$(od)/list_usage_unsafe.o
+#
+#OBJS_$(d)	:=$(COBJS_$(d))
+#TGTS_$(d)	:=$(bd)/list_usage_unsafe
+#
+#$(call append,TGT_BIN,$(d))
+#$(d): $(TGTS_$(d))
+#$(TGTS_$(d)):	$(OBJS_$(d))
+#	$(L_LINK)
+#
+#
+#
+#
+#
+#COBJS_$(d)	:=$(od)/list_usage_safe.o
+#
+#OBJS_$(d)	:=$(COBJS_$(d))
+#TGTS_$(d)	:=$(bd)/list_usage_safe
+#
+#$(call append,TGT_BIN,$(d))
+#$(d): $(TGTS_$(d))
+#$(TGTS_$(d)):	$(OBJS_$(d))
+#	$(L_LINK)
 
-OBJS_$(d)	:=$(COBJS_$(d))
-TGTS_$(d)	:=$(bd)/list_usage_unsafe
+COBJS_UNSAFE_$(d)	:=$(od)/list_usage_unsafe.o
+COBJS_SAFE_$(d)		:=$(od)/list_usage_safe.o
+
+COBJS_$(d)	:=			\
+		$(COBJS_UNSAFE_$(d))	\
+		$(COBJS_SAFE_$(d))
+
+OBJS_UNSAFE_$(d)	:=$(COBJS_UNSAFE_$(d))
+OBJS_SAFE_$(d)		:=$(COBJS_SAFE_$(d))
+
+OBJS_$(d)	:=			\
+		$(OBJS_UNSAFE_$(d))	\
+		$(OBJS_SAFE_$(d))
+
+TGT_UNSAFE_$(d)	:=$(bd)/list_usage_unsafe
+TGT_SAFE_$(d)	:=$(bd)/list_usage_safe
+
+TGTS_$(d)	:=$(TGT_UNSAFE_$(d)) $(TGT_SAFE_$(d))
 
 $(call append,TGT_BIN,$(d))
-$(d): $(TGTS_$(d))
-$(TGTS_$(d)):	$(OBJS_$(d))
+$(d): $(TGT_UNSAFE_$(d))
+$(TGT_UNSAFE_$(d)): $(TGT_SAFE_$(d)) $(OBJS_UNSAFE_$(d))
 	$(L_LINK)
 
-
-
-
-
-COBJS_$(d)	:=$(od)/list_usage_safe.o
-
-OBJS_$(d)	:=$(COBJS_$(d))
-TGTS_$(d)	:=$(bd)/list_usage_safe
-
-$(call append,TGT_BIN,$(d))
-$(d): $(TGTS_$(d))
-$(TGTS_$(d)):	$(OBJS_$(d))
+$(d): $(TGT_SAFE_$(d))
+$(TGT_SAFE_$(d)): $(OBJS_SAFE_$(d))
 	$(L_LINK)
